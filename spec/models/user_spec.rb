84829ccb1e3aeba5958bfe3a3aa61a4fdf13907a
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-11 20:53:50 +0300

    Devise user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'associations' do
-    it { is_expected.to have_many(:appointments).dependent(:destroy) }
-
-    it { is_expected.to have_many(:doctors).through(:appointments) }
-  end
-
-  describe 'validations' do
-    it { is_expected.to validate_presence_of(:phone) }
-
-    it { is_expected.to validate_presence_of(:first_name) }
-
-    it { is_expected.to validate_presence_of(:last_name) }
-
-    it { is_expected.to validate_uniqueness_of(:phone).case_insensetive }
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
