092b19ae5806b80ad5bc248c2dffce529b2ec2f1
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-12 20:28:32 +0300

    Added admin

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -59,12 +59,12 @@ Devise.setup do |config|
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
-  config.case_insensitive_keys = [:email, :phone]
+  config.case_insensitive_keys = [:phone]
 
   # Configure which authentication keys should have whitespace stripped.
   # These keys will have whitespace before and after removed upon creating or
   # modifying a user and when used to authenticate or find a user. Default is :email.
-  config.strip_whitespace_keys = [:email]
+  config.strip_whitespace_keys = [:phone]
 
   # Tell if authentication through request.params is enabled. True by default.
   # It can be set to an array that will enable params authentication only for the
@@ -161,7 +161,7 @@ Devise.setup do |config|
   config.reconfirmable = true
 
   # Defines which key will be used when confirming an account
-  # config.confirmation_keys = [:email]
+  config.confirmation_keys = [:phone]
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
