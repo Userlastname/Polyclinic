0bccda74c6ad791df65f25402b8be8541a4d6e13
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-18 21:23:44 +0300

    Config for js

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,6 @@
 
 pin "application", preload: true
 pin "sortablejs", to: "https://cdn.jsdelivr.net/npm/sortablejs@1.15.0/modular/sortable.esm.js"
+pin "react", to: "https://ga.jspm.io/npm:react@18.2.0/index.js"
+pin "react-dom", to: "https://ga.jspm.io/npm:react-dom@18.2.0/index.js"
+pin "scheduler", to: "https://ga.jspm.io/npm:scheduler@0.23.0/index.js"
