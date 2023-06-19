082376ae6096b430828458e5227ddfd51cf6c48c
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-19 16:20:01 +0300

    Created appointment controller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,21 @@
+class AppointmentsController < ApplicationController
+  def new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+
+    if @appointment.save
+      flash[:success] = "You've made an appointment"
+    else
+      render :new
+      flash[:error] = "Sorry, something went wrong"
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.permit(:user_id, :doctor_id, :recommendation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   devise_for :doctors, controllers: { registrations: 'doctors/registrations' }
   devise_for :users
   resources :categories, only: [:index, :show]
+  resources :appointments
 end
