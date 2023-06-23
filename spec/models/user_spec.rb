b71bde4ae1a35c1d8d5c45ac04fafe83f30b9b28
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-23 21:19:40 +0300

    Added test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,35 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to have_many(:appointments).dependent(:destroy) }
+
+    it { is_expected.to have_many(:doctors).through(:appointments).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:phone) }
+
+    it { is_expected.to validate_presence_of(:email) }
+
+    it { is_expected.to validate_presence_of(:first_name) }
+
+    it { is_expected.to validate_presence_of(:last_name) }
+
+    it { is_expected.to validate_uniqueness_of(:phone).case_insensitive }
+
+    it { is_expected.to allow_value('0971234567').for(:phone) }
+
+    it { is_expected.not_to allow_value('one2345678').for(:phone).with_message('should be a 10-digit number') }
+
+    it { is_expected.to allow_value('example@gmail.com').for(:email) }
+
+    it do
+      is_expected.not_to allow_value('SkinnyA$$@gmail.com')
+        .for(:email)
+        .with_message('should be a valid email address')
+    end
+  end
 end
