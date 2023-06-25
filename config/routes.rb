60f33dd62447c3473129b34a81701c1271684252
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-25 23:04:19 +0300

    Added bout us page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get 'about_us', to: "home#about"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :doctors, controllers: { registrations: 'doctors/registrations' }
