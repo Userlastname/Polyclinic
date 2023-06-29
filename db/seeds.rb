96ec183f3868ef7afd6222f29a3229c0e7d6e09a
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-29 15:05:15 +0300

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(phone: '0954481341', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(phone: '0954481341', password: 'password', password_confirmation: 'password')
+Category.create!(name: 'Immunologist')
+Category.create!(name: 'Surgeon')
+Category.create!(name: 'Therapist')
+Doctor.create!(first_name: 'House', last_name: 'Gregory', phone: '0954481343', category_id: 1)
+Doctor.create!(first_name: 'Paul', last_name: 'Industrious', phone: '0954481341', category_id: 2)
+Doctor.create!(first_name: 'Ford', last_name: 'Garryson', phone: '0954481342', category_id: 3)
