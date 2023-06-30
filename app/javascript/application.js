50aae3a3ffda973f9dcfadbe5466e1c93f2fb4d8
Paul Industrious <zhmerinka1pasha@gmail.com>
2023-06-30 23:40:43 +0300

    replaced js code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "/custom/sample"
 const dropdownButton = document.getElementById('dropdownDefaultButton');
 const dropdown = document.getElementById('dropdown');
 
